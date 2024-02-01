b22263b9337da3b3b69618cb5647fa32cb39fa74
Steven Zilberberg <srz222@gmail.com>
2024-02-01 15:56:11 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+NinjaFood
+=========
+
+This was a tutorial I followed to learn [TailWindCSS](https://tailwindcss.com) from TheNetNinja on [YouTube](https://www.youtube.com/watch?v=bxmDnn7lrnk&list=PL4cUxeGkcC9gpXORlEHjc5bgnIi5HEGhw&index=1&pp=iAQB) 
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      },
+      boxShadow: {
+        '3xl': '0 25px 50px -12px rgba(0, 0, 0, 0.50)',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
